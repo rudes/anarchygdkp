289283c25e3e134c1ab9b1ac9b689ff216268b7f
rudes <>
2023-07-25 08:46:06 +0000

    few fixes

--- bot/bot.py
+++ bot/bot.py
@@ -3,11 +3,11 @@ import discord
 import logging
 import configparser
 
-from command import Logs
+from commands import *
 from discord.ext import commands
 
 logging.basicConfig(format="%(asctime)s %(name)s:%(levelname)-8s %(message)s",
-                    filename="/var/log/gdkp_logeval.log", level=logging.INFO)
+                    filename="/var/log/anarchygdkp_bot.log", level=logging.INFO)
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 discord_log = logging.getLogger("discord")
@@ -20,6 +20,6 @@ intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(debug_guilds=[1086303502894108783], intents=intents)
 
-bot.add_cog(Logs(bot, config))
+bot.add_cog(wowlogs.Logs(bot, config))
 
 bot.run(str(os.environ["DISCORD_BOTKEY"]))
--- bot/commands/wowlogs.py
+++ bot/commands/wowlogs.py
@@ -1,73 +1,12 @@
-import os
 import discord
 import logging
 
-from wcl import WCLlogs
-from discord import ButtonStyle, SlashCommandGroup
+from util.wcl import WCLlogs
+from util.view import WoWLog
+from discord import SlashCommandGroup
 from discord.ext import commands
 
 log = logging.getLogger(__name__)
-
-class WoWLog(discord.ui.View):
-    def __init__(self, user: discord.Member, character: str, config):
-        super().__init__(timeout=172800)
-        self.user = user
-        self.character = character
-        self.config = config
-
-        url = f"https://classic.warcraftlogs.com/character/us/grobbulus/{character}"
-        button = discord.ui.Button(label="Logs", style=ButtonStyle.link, url=url)
-        self.add_item(button)
-
-    async def role_add(self, role: str):
-        try:
-            role = self.user.guild.get_role(int(self.config["ROLES"][role]))
-            if role is None:
-                log.error(f"role_add,NoneRole,role not found in guild")
-                return
-            if role in self.user.roles:
-                return
-            await self.user.add_roles(role)
-        except Exception as e:
-            log.exception(f"role_add,{type(e)},{e}")
-
-    @discord.ui.button(label="Approved", style=ButtonStyle.primary)
-    async def approve(self,
-        button: discord.ui.Button,
-        interaction: discord.Interaction,
-    ):
-        await self.role_add("Approved")
-        await interaction.response.send_message(f"{interaction.user.mention} gave {self.user.mention} Approved role.")
-        self.stop()
-
-    @discord.ui.button(label="Core", style=ButtonStyle.green)
-    async def core(self,
-        button: discord.ui.Button,
-        interaction: discord.Interaction,
-    ):
-        await self.role_add("Core")
-        await interaction.response.send_message(f"{interaction.user.mention} gave {self.user.mention} Core role.")
-        self.stop()
-
-    @discord.ui.button(label="Decline", style=ButtonStyle.red)
-    async def decline(self,
-        button: discord.ui.Button,
-        interaction: discord.Interaction,
-    ):
-        try:
-            await interaction.response.send_message(f"{interaction.user.mention} declined {self.user.mention}.")
-            try:
-                dm = await self.user.create_dm()
-                msg = ("Currently we are rostering raiders with a stronger combination "
-                       +"of Gear and Parses from the current phase of raids.\n"
-                       +f"Make some improvements to **{self.character}** "
-                       +"and come back for another shot :)")
-                await dm.send(msg)
-            except discord.Forbidden:
-                pass
-            self.stop()
-        except Exception as e:
-            log.exception(f"decline,{type(e)},{e}")
 
 class Logs(commands.Cog):
     def __init__(self, bot, config):
